36accbd19a5bfe5a463a1fe05d29d83e685c3d0e
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-07 17:22:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       
+		int a = System.in.read();
+		System.out.print(a);
+    }
+}
