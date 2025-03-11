891543f93839d971686aaef186c01b2e8da86cf3
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-11 14:56:34 +0900

    [Silver V] Title: 단어 정렬, Time: 340 ms, Memory: 22520 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -7,18 +7,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // 단어의 개수 입력
         int times = Integer.parseInt(br.readLine());
+        
+        // 알파벳 단어들 입력
         Set<String> words = new HashSet<>();
-
+        
         for (int i = 0; i < times; i++) {
             words.add(br.readLine());
         }
 
-        List<String> sortedWords = words.stream()
-                                        .sorted(Comparator.comparingInt(String::length)
-                                                .thenComparing(Comparator.naturalOrder()))
-                                        .collect(Collectors.toList());
-
+        // 정렬 기준에 따라 정렬하기
+        List<String> sortedWords = new ArrayList<>(words);
+        
+        Collections.sort(sortedWords, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b); // 길이가 같으면 사전순 정렬
+            }
+            return a.length() - b.length(); // 길이순 정렬
+        });
+        
         for (String word : sortedWords) {
             bw.write(word);
             bw.newLine();
