97ba5e934637c1ed01a69d6cab2eb7551a355d2b
RIWON <121172065+awrion3@users.noreply.github.com>
2025-03-23 16:15:16 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine()," ");
+		
+		int n = Integer.parseInt(token.nextToken());
+		
+		double score[] = new double[n]; 
+		double avg, max = 0, sum = 0;
+		
+		token = new StringTokenizer(br.readLine());
+        
+		for(int i=0;i<n;i++) {
+			score[i] = Double.parseDouble(token.nextToken());
+			
+			if(score[i]>max) 
+				max = score[i]; 
+		}
+		
+		for(int i=0;i<score.length;i++) {
+			score[i] = (score[i]/max)*100;
+			sum += score[i];
+		}
+		br.close();
+		
+		avg = sum / n;
+		
+		System.out.println(avg);
+	}
+}
